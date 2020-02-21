4f5e719aef6ac089976ebc2be5dd7bf3d8e68810
Gabor Sinka <gabor.sinka-ext@continental-corporation.com>
2020-02-21 10:01:18 +0100

    Serilog overload in default app config

--- src2/Acnys/Acnys.Core.AspNet/AppBuilderExtensions.cs
+++ src2/Acnys/Acnys.Core.AspNet/AppBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Acnys.Core.AspNet.Eventing;
 using Acnys.Core.AspNet.Request;
@@ -19,71 +20,74 @@ namespace Acnys.Core.AspNet
     {
         public static IHostBuilder PrebuildDefaultApp(this IHostBuilder hostBuilder)
         {
+
             return hostBuilder
-                
                 .AddSerilog((context, config) => config
                     .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss+fff}{EventType:x8} {Level:u3}][{App}] {Message:lj} <-- [{SourceContext}]{NewLine}{Exception}", theme: AnsiConsoleTheme.Code)
                     .Enrich.FromLogContext()
                     .Enrich.WithProperty("App", "SMC")
                     .MinimumLevel.Verbose()
                     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                    .MinimumLevel.Override("System", LogEventLevel.Warning))
+                    .MinimumLevel.Override("System", LogEventLevel.Warning));
+        }
 
-                .AddAutofac()
+        public static IHostBuilder PrebuildDefaultApp(this IHostBuilder hostBuilder, Action<HostBuilderContext, LoggerConfiguration> logger)
+        {
+            return hostBuilder
+                    
+                    .AddSerilog(logger)
+                    .AddAutofac()
 
-                .AddHealthChecks((context, builder) => builder
-                    .AddCheck("Self", () => HealthCheckResult.Healthy(), new List<string> {"Liveness"})
-                )
-                .AddHttpMetrics()
-                .AddOpenApiDocumentation(
-                    (context, app) => context.Configuration.Bind("Application", app),
-                    (context, sso) => context.Configuration.Bind("SingleSignOn", sso))
-                .AddRequests()
-                .AddRequestValidation()
-                
-                .AddHttpRequestHandler()
-                .AddEventing()
-                
-                //.AddRabbitService((context, configuration) => context.Configuration.Bind("Rabbit", configuration))
+                    .AddHealthChecks((context, builder) => builder
+                        .AddCheck("Self", () => HealthCheckResult.Healthy(), new List<string> { "Liveness" })
+                    )
+                    .AddHttpMetrics()
+                    .AddOpenApiDocumentation(
+                        (context, app) => context.Configuration.Bind("Application", app),
+                        (context, sso) => context.Configuration.Bind("SingleSignOn", sso))
+                    .AddRequests()
+                    .AddRequestValidation()
 
-                .ConfigureContainer<ContainerBuilder>((context, builder) => { })
-                
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddControllers().AddApplicationPart(Assembly.GetEntryAssembly()).AddControllersAsServices();
-                    //services.Configure<RabbitServiceConfiguration>(context.Configuration.GetSection("Rabbit"));
-                })
+                    .AddHttpRequestHandler()
+                    .AddEventing()
 
-                .ConfigureWebHostDefaults(builder => builder.Configure((context, app) =>
-                {
-                    if (context.HostingEnvironment.IsDevelopment())
+                    .ConfigureContainer<ContainerBuilder>((context, builder) => { })
+
+                    .ConfigureServices((context, services) =>
                     {
-                        app.UseDeveloperExceptionPage();
-                    }
+                        services.AddControllers().AddApplicationPart(Assembly.GetEntryAssembly()).AddControllersAsServices();
+                    })
 
-                    app.UseRouting();
-                    app.UseAuthorization();
-
-                    var appSettings = new ApplicationOptions();
-                    context.Configuration.Bind("Application", appSettings);
-
-                    var ssoSettings = new SingleSignOnOptions();
-                    context.Configuration.Bind("SingleSignOn", ssoSettings);
-
-                    var openApiSettings = new OpenApiDocumentationOptions() { Path = "/swagger" };
-
-                    app.AddOpenApiDocumentation(appSettings, ssoSettings, openApiSettings);
-
-                    app.AddReadiness();
-                    app.AddLiveness();
-
-                    app.UseEndpoints(endpoints =>
+                    .ConfigureWebHostDefaults(builder => builder.Configure((context, app) =>
                     {
-                        endpoints.MapControllers();
-                        endpoints.MapHttpRequestHandler("api");
-                    });
-                }))
-            ;
+                        if (context.HostingEnvironment.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+
+                        app.UseRouting();
+                        app.UseAuthorization();
+
+                        var appSettings = new ApplicationOptions();
+                        context.Configuration.Bind("Application", appSettings);
+
+                        var ssoSettings = new SingleSignOnOptions();
+                        context.Configuration.Bind("SingleSignOn", ssoSettings);
+
+                        var openApiSettings = new OpenApiDocumentationOptions() { Path = "/swagger" };
+
+                        app.AddOpenApiDocumentation(appSettings, ssoSettings, openApiSettings);
+
+                        app.AddReadiness();
+                        app.AddLiveness();
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                            endpoints.MapHttpRequestHandler("api");
+                        });
+                    }))
+                ;
         }
     }
 }
