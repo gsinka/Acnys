de8f97298fbe897ae2ad53be83894e1ab583936c
Gábor Sinka <gabor.sinka@erstcom.hu>
2020-02-29 19:02:23 +0100

    random string generator

---
+++ src/Acnys/Acnys.Core.Tests/RandomStringTests.cs
@@ -0,0 +1,23 @@
+﻿using Acnys.Core.Helper;
+using Xunit;
+
+namespace Acnys.Core.Tests
+{
+    public class RandomStringTests
+    {
+        [Fact]
+        public void Random_postfix()
+        {
+            var str = "test";
+            var pre = str.AddRandomPrefix();
+            var post = str.AddRandomPostfix();
+
+            Assert.StartsWith(str, post);
+            Assert.Equal(15, pre.Length);
+            
+            Assert.EndsWith(str, pre);
+            Assert.Equal(15, post.Length);
+
+        }
+    }
+}
