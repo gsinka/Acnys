c97b1230e68617c8709445d5cb9ac509f353fd89
Bence Mihucza (uids6410) <bence.mihucza-ext@continental-corporation.com>
2020-04-02 14:35:36 +0200

    Add extension for get correlation id from request header.

---
+++ src/Acnys/Acnys.Core.AspNet.Request/HttpRequestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Acnys.Core.AspNet.Request
+{
+    public static class HttpRequestExtensions
+    {
+        public static Guid GetCorrelationId(this HttpRequest request)
+        {
+            return request.Headers.TryGetValue("correlationId", out var correlationIdString) ? Guid.Parse(correlationIdString) : Guid.NewGuid();
+        }
+    }
+}
