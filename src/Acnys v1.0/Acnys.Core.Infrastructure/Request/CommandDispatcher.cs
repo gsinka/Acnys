32b77bb8ba9456d2fecd37cd79c7ff6979d2798e
Gabor Sinka <gabor.sinka-ext@continental.com>
2020-04-12 14:08:40 +0200

    command dispatcher tests

---
+++ src/Acnys v1.0/Acnys.Core.Infrastructure/Request/CommandDispatcher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Acnys.Core.Abstractions;
+using Acnys.Core.Application.Abstractions;
+using Acnys.Core.Infrastructure.Abstractions;
+using Autofac;
+using Serilog;
+
+namespace Acnys.Core.Infrastructure.Request
+{
+    /// <summary>
+    /// Autofac based command dispatcher
+    /// </summary>
+    public class CommandDispatcher : IDispatchCommand
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger _log;
+
+        public CommandDispatcher(ILifetimeScope scope, ILogger log)
+        {
+            _scope = scope;
+            _log = log;
+        }
+
+        /// <summary>
+        /// Dispatch command to the registered handler
+        /// </summary>
+        /// <typeparam name="T">Type of command</typeparam>
+        /// <param name="command">Command to dispatch</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns></returns>
+        public Task Dispatch<T>(T command, IDictionary<string, object> arguments = null, CancellationToken cancellationToken = default) where T : ICommand
+        {
+            return Task.Run(async() =>
+            {
+                _log.Debug("Dispatching command {commandType}", typeof(T).Name);
+
+                _log.Verbose("Command object: {@command}", command);
+
+                _log.Verbose("Lifetime scope {scopeId} created for command", _scope.GetHashCode());
+
+                try
+                {
+                    var commandType = typeof(T);
+                    var handlerType = typeof(IHandleCommand<>).MakeGenericType(commandType);
+                    _log.Verbose("Looking handler with type {handlerType}", handlerType);
+
+                    var handler = (IHandleCommand<T>)_scope.Resolve(handlerType);
+                            
+                    _log.Verbose("Handling {commandType} with {handler}", commandType.Name, handler.GetType().Name);
+                    await handler.Handle(command, arguments, cancellationToken);
+
+                    _log.Debug("Command dispatch completed successfully");
+                }
+                catch (Exception exception)
+                {
+                    _log.Error(exception, "Command dispatch failed");
+                    throw;
+                }
+                finally
+                {
+                    _log.Verbose("Ending lifetime scope {scopeId}", _scope.GetHashCode());
+                }
+            }, cancellationToken);
+        }
+    }
+}
---
+++ src/Acnys v1.0/Acnys.Core.Tests/CommandDispatcherTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Acnys.Core.Application.Abstractions;
+using Acnys.Core.Helper;
+using Acnys.Core.Infrastructure.Abstractions;
+using Acnys.Core.Infrastructure.Request;
+using Autofac;
+using Serilog;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Acnys.Core.Tests
+{
+    public class CommandDispatcherTests
+    {
+        private readonly IContainer _container;
+        private readonly TestCommandHandler _testCommandHandler = new TestCommandHandler();
+
+        public CommandDispatcherTests(ITestOutputHelper outputHelper)
+        {
+            var builder = new ContainerBuilder();
+
+            Log.Logger = new LoggerConfiguration().WriteTo.TestOutput(outputHelper).MinimumLevel.Verbose().CreateLogger();
+            builder.RegisterInstance(Log.Logger).As<ILogger>().SingleInstance();
+
+            builder.RegisterCommandDispatcher();
+            builder.RegisterInstance(_testCommandHandler).AsImplementedInterfaces().SingleInstance();
+
+            _container = builder.Build();
+        }
+
+        [Fact]
+        public async Task Test_command_dispatcher_with_valid_command()
+        {
+            var dispatcher = _container.Resolve<IDispatchCommand>();
+            
+            var command = new TestCommand();
+            var correlationId = Guid.NewGuid();
+            var arguments = new Dictionary<string, object>().UseCorrelationId(correlationId);
+
+            await dispatcher.Dispatch(command, arguments);
+
+            Assert.Equal(command, _testCommandHandler.Command);
+            Assert.Equal(arguments, _testCommandHandler.Arguments);
+        }
+        
+        [Fact]
+        public async Task Test_command_dispatcher_with_invalid_command()
+        {
+            var dispatcher = _container.Resolve<IDispatchCommand>();
+            var command = new TestCommand(true);
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await dispatcher.Dispatch(command));
+        }
+
+        private class TestCommand : Command
+        {
+            public bool ThrowException { get; }
+
+            public TestCommand(bool throwException = false)
+            {
+                ThrowException = throwException;
+            }
+        }
+
+        private class TestCommandHandler : IHandleCommand<TestCommand>
+        {
+            public TestCommand Command;
+            public IDictionary<string, object> Arguments;
+
+            public Task Handle(TestCommand command, IDictionary<string, object> arguments = null, CancellationToken cancellationToken = default)
+            {
+                if (command.ThrowException) throw new InvalidOperationException();
+
+                Command = command;
+                Arguments = arguments;
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
