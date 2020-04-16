4b9a38c1c41732d72a3963bd3fe0af6884924cf0
Gabor Sinka <gabor.sinka-ext@continental.com>
2020-04-16 10:04:42 +0200

    aspnet test host

--- src/Acnys v1.0/Acnys.Core.Infrastructure/Hosting/HostBuilderExtensions.cs
+++ src/Acnys v1.0/Acnys.Core.Infrastructure/Hosting/HostBuilderExtensions.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Acnys.Core.Abstractions;
+using Autofac;
 using Microsoft.Extensions.Hosting;
 
 namespace Acnys.Core.Infrastructure.Hosting
 {
     public static class HostBuilderExtensions
     {
+        public static IHostBuilder AddClock<T>(this IHostBuilder hostBuilder) where T : IClock
+        {
+            hostBuilder.ConfigureContainer<ContainerBuilder>((context, builder) =>
+            {
+                builder.RegisterType<T>().As<IClock>().SingleInstance();
+            });
+
+            return hostBuilder;
+
+        }
     }
 }
---
+++ src/Acnys v1.0/Acnys.Core.Tests/AspNetTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Acnys.AspNet;
+using Acnys.Core.Abstractions;
+using Acnys.Core.Infrastructure.Hosting;
+using Acnys.Core.Infrastructure.Serilog;
+using Acnys.Core.Services;
+using Acnys.Core.Tests.Helpers;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Acnys.Core.Tests
+{
+    public class AspNetTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private ILifetimeScope _container;
+
+
+        public AspNetTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task Test_host_configuration()
+        {
+            await BuildHost();
+
+            Assert.IsType<ComputerClock>(_container.Resolve<IClock>());
+        }
+
+        private async Task BuildHost()
+        {
+            var host = new HostBuilder()
+
+                .AddSerilog((context, configuration) => configuration.ConfigureLogForTesting(_testOutputHelper))
+                .AddAutofac()
+                .AddClock<ComputerClock>()
+                .AddHttpRequestHandler()
+
+                .ConfigureContainer<ContainerBuilder>((context, builder) =>
+                {
+                    builder.RegisterType<ComputerClock>().AsImplementedInterfaces();
+
+                })
+                .ConfigureServices((context, services) =>
+                {
+
+                })
+                .ConfigureWebHost(builder =>
+                {
+                    builder.Configure((context, app) =>
+                    {
+                        app.AddErrorHandling();
+                        app.UseRouting();
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                            endpoints.MapHttpRequestHandler("api");
+                        });
+                    });
+                })
+
+                .Build();
+
+            await host.RunAsync();
+
+            _container = host.Services.GetAutofacRoot();
+        }
+    }
+}
