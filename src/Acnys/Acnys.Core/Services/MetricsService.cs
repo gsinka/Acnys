7aa058a1285a3d7e47b717d149fcd5ab1043004b
David J. Mallar <mallar.david@gmail.com>
2020-09-18 09:01:25 +0200

    Renamings in the MetricsService class

--- src/Acnys/Acnys.Core/Services/MetricsService.cs
+++ src/Acnys/Acnys.Core/Services/MetricsService.cs
@@ -10,50 +10,9 @@ namespace Acnys.Core.Services
 {
     public class MetricsService
     {
-        private readonly Summary _summary = Prometheus.Metrics.CreateSummary("microservice_summaries", "Auto-generated summary metrics", new SummaryConfiguration() { LabelNames = new[] { "namespace", "name", "type" } });
+        private readonly Summary _summary = Metrics.CreateSummary("microservice_summaries", "Auto-generated summary metrics", new SummaryConfiguration() { LabelNames = new[] { "namespace", "method", "metric_type" } });
 
-        private readonly Counter _counter = Prometheus.Metrics.CreateCounter("microservice_counters", "Auto-generated counter metrics", new CounterConfiguration() { LabelNames = new[] { "namespace", "name", "type" } });
-
-        //private readonly List<Counter> _counters = new List<Counter>();
-        //private readonly List<Summary> _summaries = new List<Summary>();
-
-        //public Counter GetCounter<T>()
-        //{
-        //    var name = _counterName + CalculateName(typeof(T).Name);
-        //    var metric = _counters.FirstOrDefault(c => c.Name == name);
-        //    if (metric == null)
-        //    {
-        //        metric = Prometheus.Metrics.CreateCounter(name, _counterText + typeof(T).FullName, new CounterConfiguration()
-        //        {
-        //            LabelNames = new[] { "name" }
-        //        });
-        //        _counters.Add(metric);
-        //    }
-        //    return metric;
-        //}
-        //public Summary GetSummary<T>()
-        //{
-        //    var name = "";
-        //    var metric = _summaries.FirstOrDefault(c => c.Name == name);
-        //    if (metric == null)
-        //    {
-        //        metric = Prometheus.Metrics.CreateSummary(name, _summaryText + typeof(T).FullName,
-        //            new SummaryConfiguration()
-        //            {
-        //                LabelNames = new[] { "name" },
-        //            });
-        //        _summaries.Add(metric);
-        //    }
-        //    return metric;
-        //}
-
-        // input:   Test2.Almafa.TestCommandFirst
-        // output:  test2_almafa__test_command_first
-        private string CalculateName(string n)
-        {
-            var seperated = n.SelectMany((c, i) => i != 0 && char.IsUpper(c) && !char.IsUpper(n[i - 1]) ? new char[] { ' ', c } : new char[] { c });
-            return new string(seperated.ToArray()).ToLower().Trim().Replace('.', '_').Replace(' ', '_');
-        }
+        private readonly Counter _counter = Metrics.CreateCounter("microservice_counters", "Auto-generated counter metrics", new CounterConfiguration() { LabelNames = new[] { "namespace", "method", "metric_type" } });
 
         public void ObserveInSummary<T>(double val, string type = "")
         {
