78126cd11d90e5db167cbd0d6add2f85924acb3f
David J. Mallar <mallar.david@gmail.com>
2020-09-23 10:12:13 +0200

    cleaned up comments

--- src/Acnys/WebApplication1/TestCommandHandler.cs
+++ src/Acnys/WebApplication1/TestCommandHandler.cs
@@ -29,28 +29,14 @@ namespace WebApplication1
 
         public async Task Handle(TestCommand command, IDictionary<string, object> arguments = null, CancellationToken cancellationToken = default)
         {
-            //var spanBuilder1 = _tracer.BuildSpan("operation 1");
-            //using (var scope = spanBuilder1.StartActive(true))
-            //{
-            //    scope.Span.Log("ok");
-            //    scope.Span.SetTag(new OpenTracing.Tag.StringTag("Name"), "A");
-            //    var spanBuilder2 = _tracer.BuildSpan("operation 2");
-            //    using (var scope2 = spanBuilder2.StartActive(true))
-            //    {
-            //        scope2.Span.Log("ok");
-            //        scope2.Span.SetTag(new OpenTracing.Tag.StringTag("Name"), "B");
-            //    }
-            //}
-
+            
             var testEvent = new TestEvent(command.Data);
             var args = new Dictionary<string, object>()
             {
                 { "test", "test" },
                 { "int", 1 },
                 { "Routing", "test.level" },
-                //{ "uber-trace-id", (arguments["uber-trace-id"].ToString())}
             }.EnrichWithCorrelation(command, arguments);
-            //arguments.Add("uber-trace-id", new StringValues(traceExtendedHeaders["uber-trace-id"]));
             var testEvent2 = new TestEvent(command.Data);
 
             var args2 = new Dictionary<string, object>()
@@ -58,7 +44,6 @@ namespace WebApplication1
                 { "test", "test2" },
                 { "int", 2 },
                 { "RoutingKey", "test.level" },
-                //{ "uber-trace-id", (arguments["uber-trace-id"].ToString())}
             }.EnrichWithCorrelation(command, arguments);
 
             await _eventPublisher.Publish(testEvent, args, cancellationToken);
