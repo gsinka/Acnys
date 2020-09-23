1e1c9bb654c82b4faf80f8065de0c8fc3a33c0ce
David J. Mallar <mallar.david@gmail.com>
2020-09-23 13:34:03 +0200

    Tracing setup update 2

--- src/Acnys/Acnys.Core.AspNet/Extensions/TracingExtensions.cs
+++ src/Acnys/Acnys.Core.AspNet/Extensions/TracingExtensions.cs
@@ -5,6 +5,7 @@ using Jaeger;
 using Jaeger.Samplers;
 using Jaeger.Senders;
 using Jaeger.Senders.Thrift;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -18,15 +19,61 @@ namespace Acnys.Core.AspNet.Extensions
     public static class TracingExtensions
     {
 
-        public static IHostBuilder AddTracing(this IHostBuilder hostBuilder, string host = "localhost", int port = 6831, bool addOpenTracing = false)
+        public static IHostBuilder AddTracing(this IHostBuilder hostBuilder, Action<HostBuilderContext, IServiceCollection> configuration = null, bool addOpenTracing = false)
         {
+
+            if (configuration == null) configuration = (context, services) =>
+             {
+                 services.AddSingleton<ITracer>(serviceProvider =>
+                 {
+                     var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                     Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
+                         .RegisterSenderFactory<ThriftSenderFactory>();
+
+                     var udpSender = new Configuration.SenderConfiguration(loggerFactory)
+                         .WithAgentHost(context.Configuration.GetValue<string>("Tracing:Host") ?? "localhost")
+                         .WithAgentPort(context.Configuration.GetValue<int?>("Tracing:Port") ?? 6831)
+                         .GetSender();
+
+                     //var config = Configuration.FromEnv(loggerFactory);
+
+                     var tracer = new Tracer.Builder(Assembly.GetEntryAssembly().GetName().Name)
+                          .WithLoggerFactory(loggerFactory)
+                          .WithSampler(new ConstSampler(true))
+                          .Build();
+
+                     if (!GlobalTracer.IsRegistered())
+                     {
+                         GlobalTracer.Register(tracer);
+                     }
+
+                     return tracer;
+                 });
+
+                 if (addOpenTracing)
+                 {
+                     // Useless default http tracing
+                     services.AddOpenTracing();
+                 }
+
+             };
 
             return hostBuilder.ConfigureContainer<ContainerBuilder>((context, containerBuilder) =>
             {
                 containerBuilder.AddEventTracingBehaviour();
                 containerBuilder.AddCommandTracingBehaviour();
             }).
-            ConfigureServices((context, services) =>
+            ConfigureServices(configuration);
+        }
+    }
+}
+
+
+
+/*
+ 
+(context, services) =>
             {
                 services.AddSingleton<ITracer>(serviceProvider =>
                 {
@@ -36,8 +83,8 @@ namespace Acnys.Core.AspNet.Extensions
                         .RegisterSenderFactory<ThriftSenderFactory>();
 
                     var udpSender = new Configuration.SenderConfiguration(loggerFactory)
-                        .WithAgentHost(host)
-                        .WithAgentPort(port)
+                        .WithAgentHost(context.Configuration.GetValue<string>("Tracing:Host") ?? "localhost")
+                        .WithAgentPort(context.Configuration.GetValue<int?>("Tracing:Port") ?? 6831)
                         .GetSender();
 
                     //var config = Configuration.FromEnv(loggerFactory);
@@ -61,7 +108,5 @@ namespace Acnys.Core.AspNet.Extensions
                     services.AddOpenTracing();
                 }
 
-            });
-        }
-    }
-}
+            }
+ */
